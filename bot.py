b49ddc7fea0027cca00e2943e35b5a7a7d7f1bd1
Arnab Paryali <arnabparyali@gmail.com>
2020-06-07 13:53:50 +0530

    Fixed Echo of Multiple Space separated words

--- bot.py
+++ bot.py
@@ -2,16 +2,18 @@ import telebot
 
 bot = telebot.TeleBot("1153023482:AAHqxLOLkXs6XdDb9-tQ-hOANeJVoHiYFIw")
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Welcome to @AnimeDB_Bot")
 @bot.message_handler(func=lambda e: e.text.startswith('/echo'))
 def msg(message):
     m = message.text.split(None)
+#   print(type(m))
     if len(m)==1:
       bot.reply_to(message,"Nothing to echo")
     else:
-      bot.reply_to(message,m[2] )
+      msg = ' '.join(m[1:])
+      bot.reply_to(message,msg)
 
 bot.polling()
     
