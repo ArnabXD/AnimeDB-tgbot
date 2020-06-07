de26c1f1eef182fdf50401d2252a24a7a4741124
Soumya665 <soumyadipbera16@gmail.com>
2020-06-07 17:59:43 +0530

    Removed Comment

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ bot = telebot.TeleBot("1153023482:AAHqxLOLkXs6XdDb9-tQ-hOANeJVoHiYFIw")
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "Welcome to @AnimeDB_Bot")
+
 @bot.message_handler(func=lambda e: e.text.startswith('/echo'))
 def msg(message):
     m = message.text.split(None)
-#   print(type(m))
     if len(m)==1:
       bot.reply_to(message,"Nothing to echo")
     else:
