14266011230bcd5e28a3d1da62161c38e17cf3df
Arnab Paryali <arnabparyali@gmail.com>
2020-06-08 23:17:44 +0530

    Added Manga Searching Feature

--- search.py
+++ search.py
@@ -3,8 +3,8 @@ import requests,json
 headers = { 'Accept': 'application/vnd.api+json',
 'Content-Type': 'application/vnd.api+json'}
 
-def search_anime(key):
-  response = requests.get('https://kitsu.io/api/edge/anime?filter[text]='+key,headers=headers)
+def search_anime(type,key):
+  response = requests.get('https://kitsu.io/api/edge/'+type+'?filter[text]='+key,headers=headers)
   result = json.loads(response.text)
   if result["meta"]["count"]!=0:
     return result["data"][0]
