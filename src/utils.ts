fab65173098c791c456a0e6e0ad171530b5410a7
ArnabXD <arnabxd@pm.me>
2021-02-08 17:41:15 +0530

    fix MEDIA_CAPTION_TOO_LONG

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import { mediaResponse } from './anilist/getMedia';
 
 export const parseMedia = (resp: mediaResponse, type: "ANIME" | "MANGA" = "ANIME"): string => {
     let { Media } = resp.data;
+    console.log(JSON.stringify(Media))
     let synopsis = Media.description && Media.description.replace(/<\/br>/g, '').replace(/<br>/g, '');
     if (synopsis && synopsis.length > 700) {
         synopsis = synopsis.substr(0, 700) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>`
@@ -12,8 +13,12 @@ export const parseMedia = (resp: mediaResponse, type: "ANIME" | "MANGA" = "ANIME
         `<b>• Average Rating :</b> ${Media.averageScore} <a href="https://img.anili.st/media/${Media.id}">&#8205;</a>\n` +
         `<b>• Status :</b> ${Media.status}\n` +
         `<b>• Genres :</b> ${Media.genres.join(", ")}\n` +
-        `<b>• Studios :</b> ${Media.studios.nodes.map(data => data.name).join(', ')}\n` +
-        `\n` +
-        `<b>• Synopsis :</b> <i>${synopsis}</i>`;
+        (Media.studios.nodes.length ? `<b>• Studios :</b> ${Media.studios.nodes.map(data => data.name).join(', ')}\n` : '') +
+        `\n`
+    if (synopsis) {
+        synopsis = synopsis.length > (1000 - message.length) ? synopsis.substring(0, 1000 - message.length) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>` : synopsis
+        message += `<b>• Synopsis :</b> <i>${synopsis}</i>`;
+    }
+
     return message;
 }
