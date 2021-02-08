8f878a1b72b29e914a429023ddf44ab384e93874
ArnabXD <arnabxd@pm.me>
2021-02-08 17:23:52 +0530

    reduce synopsis length

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,8 @@ import { mediaResponse } from './anilist/getMedia';
 export const parseMedia = (resp: mediaResponse, type: "ANIME" | "MANGA" = "ANIME"): string => {
     let { Media } = resp.data;
     let synopsis = Media.description && Media.description.replace(/<\/br>/g, '').replace(/<br>/g, '');
-    if (synopsis && synopsis.length > 750) {
-        synopsis = synopsis.substr(0, 750) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>`
+    if (synopsis && synopsis.length > 700) {
+        synopsis = synopsis.substr(0, 700) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>`
     }
     let message =
         `<b>${Media.title.romaji}</b> (${Media.format})\n\n` +
