568e8da5ab5f85950ebe25a16d468d75ec07c16f
ArnabXD <arnabxd@pm.me>
2021-02-09 23:35:21 +0530

    make prev commit actually working

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,8 @@ import { mediaResponse } from './anilist/getMedia';
 
 export const parseMedia = (resp: mediaResponse, type: "ANIME" | "MANGA" = "ANIME"): string => {
     let { Media } = resp.data;
-
     let synopsis = Media.description && Media.description.replace(/<\/br>/g, '').replace(/<br>/g, '');
-    if (synopsis && synopsis.length > 700) {
-        synopsis = synopsis.substr(0, 700) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>`
-    }
+
     let message =
         `<b>${Media.title.romaji}</b> (${Media.format})\n\n` +
         `${Media.title.english && (Media.title.english !== Media.title.romaji) ? '<b>• English Title :</b> ' + Media.title.english + '\n' : ''}` +
@@ -15,6 +12,7 @@ export const parseMedia = (resp: mediaResponse, type: "ANIME" | "MANGA" = "ANIME
         `<b>• Genres :</b> ${Media.genres.join(", ")}\n` +
         (Media.studios.nodes.length ? `<b>• Studios :</b> ${Media.studios.nodes.map(data => data.name).join(', ')}\n` : '') +
         `\n`
+
     if (synopsis) {
         synopsis = synopsis.length > (900 - message.length) ? synopsis.substring(0, 900 - message.length) + `... <a href="https://anilist.co/${type.toLowerCase()}/${Media.id}">Read More</a>` : synopsis
         message += `<b>• Synopsis :</b> <i>${synopsis}</i>`;
