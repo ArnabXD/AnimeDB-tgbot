268584a73a2c6197c7da9405811339ef354d693b
ArnabXD <arnabxd@pm.me>
2021-02-09 23:36:02 +0530

    try catch

--- src/middlewares/ping.ts
+++ src/middlewares/ping.ts
@@ -3,7 +3,11 @@ import { Context, MiddlewareFn } from 'telegraf';
 const ping: MiddlewareFn<Context> = async (ctx, next) => {
     const start = new Date().getTime()
     ctx.state.time = start
-    await next()
+    try {
+        await next()
+    } catch (err) {
+        console.error(err.message);
+    }
     const ms = new Date().getTime() - start
     console.log(`${new Date().toTimeString()} >> Update ID: ${ctx.update.update_id} || Response time: ${ms} ms || User: ${ctx.from?.first_name} (${ctx.from?.id})`)
 }
