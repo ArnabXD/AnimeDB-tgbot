9e00657bd5b9efd7dbd77d2c61058d2db8e9b5d4
Arnab Paryali <arnabxd@pm.me>
2021-02-18 09:56:17 +0530

    Delete build directory

--- build/commands/id.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const telegraf_1 = require("telegraf");
-const id = telegraf_1.Composer.command('id', async (ctx) => {
-    var _a;
-    return await ctx.replyWithHTML(`<b>User ID :</b> <i>${(_a = ctx.from) === null || _a === void 0 ? void 0 : _a.id}</i>\n` +
-        (ctx.message.chat.type !== 'private' ? `<b>Chat ID :</b> <i>${ctx.message.chat.id}</i>` : ''));
-});
-exports.default = id;
--- build/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const telegraf_1 = require("telegraf");
-const cmd_1 = __importDefault(require("./cmd"));
-const ping_1 = __importDefault(require("./ping"));
-exports.default = new telegraf_1.Composer().use(cmd_1.default, ping_1.default);
