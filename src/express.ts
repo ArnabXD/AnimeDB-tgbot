3e1d66d44d956549d27e9b0a6cbd5cc084e27439
ArnabXD <arnabxd@proton.me>
2022-06-07 14:12:28 +0530

    feat: express to koa

--- src/express.ts
+++ src/express.ts
@@ -1,4 +1,4 @@
-import Koa from 'koa';
+import express from 'express';
 import { Bot, webhookCallback } from 'grammy';
 import Middlewares from './middlewares';
 import Commands from './commands';
@@ -11,10 +11,15 @@ if (!token) {
   throw new Error('BOT_TOKEN is not defined');
 }
 
-const app = new Koa();
+const app = express();
 const bot = new Bot(token);
 
 bot.use(Middlewares, Commands, Handlers);
-app.use(webhookCallback(bot, 'koa'));
+app.get(`/${token}`, async (req, res) => {
+  const fullUrl = `https://${req.get('host')}${req.originalUrl}`;
+  await bot.api.setWebhook(fullUrl);
+  return res.json({ success: true });
+});
+app.post(`/${token}`, express.json(), webhookCallback(bot, 'express'));
 
 app.listen(process.env.PORT || 3000);
